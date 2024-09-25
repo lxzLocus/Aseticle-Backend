b4c97f097c6c84317053b49f8e1d318d66ef676b
lxzLocus <75218672+lxzLocus@users.noreply.github.com>
2024-09-25 03:28:04 +0000

    [WIP] grpc対応 scholar module対応前

--- config/app.py
+++ config/app.py
@@ -1,43 +1,13 @@
-from flask import Flask, request, jsonify, make_response
-from flask_cors import CORS
+import sys
 import asyncio
 from dotenv import load_dotenv
-import os
-import sys
-import threading
 
 sys.path.append('/app/app')  
 from googlescholar_searcher import scraping_main  # モジュールをインポート
-from config import Localization
-# gRPC サーバーをインポート
-from grpc_server import serve as grpc_serve
-
+from grpc_server import serve as grpc_serve  # gRPC サーバーをインポート
 
 load_dotenv()
-app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": os.environ.get("FRONT_API_URL")}})
-
-@app.route("/")
-def index():
-    return "Welcome to ASEticle."
-
-@app.route('/api/execute', methods=['GET'])
-def execute_route():
-    if request.args.get('params') is not None:
-        params = request.args.get('params')
-    else:
-        return jsonify({"error": Localization.get('config.routes.bad_request')}), 400
-
-    result = asyncio.run(scraping_main(params))
-
-    return result
-
-def run_flask():
-    app.run(port=8080, debug=True)
 
 if __name__ == "__main__":
-    # Flask と gRPC のサーバーを同時に実行
-    grpc_thread = threading.Thread(target=grpc_serve)
-    grpc_thread.start()
-
-    # run_flask()
+    # gRPC サーバーのみを実行
+    grpc_serve()
